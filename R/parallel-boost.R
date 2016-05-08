322e2bc31a05fb563b4db0609de3f5266fba58a2
Jacob Carey <jacobcvt12@gmail.com>
2016-05-07 21:39:10 -0400

    Not quite working, but partially there

--- R/parallel-boost.R
+++ R/parallel-boost.R
@@ -1,12 +1,9 @@
-rnorm_par <- function(n=1, chains=1, seed=1) {
+rnorm_par <- function(n=1, chains=1, seed=-1) {
     chains <- matrix(0, n, chains)
 
     for (i in 1:chains) {
-        if (seed >= 1) {
-            chains[, i] <- rnorm_cpp(n, seed * i)
-        } else {
-            chains[, i] <- rnorm_cpp(n)
-        }
+        X <- rnorm_cpp(n=n, seed=seed * i)
+        chains[, i] <- x
     }
 
     return(chains)
