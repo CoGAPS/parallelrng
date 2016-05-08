d4df5bccbf7c06797615033c8dbe476d2d4996a2
Jacob Carey <jacobcvt12@gmail.com>
2016-05-07 20:47:44 -0400

    Translate to class structure to correct issue
    Now uses the same RNG for subsequent draws

--- src/rng.cpp
+++ src/rng.cpp
@@ -2,29 +2,36 @@
 #include <boost/random/normal_distribution.hpp>
 #include <boost/random/mersenne_twister.hpp>
 #include <Rcpp.h>
-#include <vector>
+
+class Rnorm_class {
+  private:
+    boost::mt19937 rng;
+    boost::normal_distribution<> nd;
+    boost::variate_generator<boost::mt19937&, boost::normal_distribution<> > norm_rnd;
+
+  public:
+    Rnorm_class(int seed=1, double mean=0.0, double variance=1.0):
+       rng(seed), 
+       nd(mean, variance),
+       norm_rnd(rng, nd) { }
+
+    ~Rnorm_class() { }
+
+    Rcpp::NumericVector draw(int n = 1) {
+        Rcpp::NumericVector samples(n);
+
+        for (int i = 0; i < n; ++i) {
+            samples[i] = norm_rnd();
+        }
+
+        return samples;
+    }
+};
 
 // [[Rcpp::export]]
-Rcpp::NumericVector rnorm_boost(int n=1, int seed=-1) {
-    int set_seed;
+void test_boost(int seed=1) {
+    Rnorm_class draws(seed);
 
-    if (seed < 1) {
-        set_seed = std::time(0);
-    } else {
-        set_seed = seed;
-    }
-
-    boost::mt19937 rng(set_seed);
-
-    boost::normal_distribution<> nd(0.0, 1.0);
-
-    boost::variate_generator<boost::mt19937 &, boost::normal_distribution<> > norm_rnd(rng, nd);
-
-    Rcpp::NumericVector draws(n);
-
-    for (int i = 0; i < n; ++i) {
-        draws[i] = norm_rnd();
-    }
-
-    return draws;
+    Rf_PrintValue(draws.draw(4));
+    Rf_PrintValue(draws.draw(4));
 }
